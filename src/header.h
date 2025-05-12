561cef2dc2e08d5d2dce7b1ed5ac4394e3f09858
kh3rld <kherld.hussein@gmail.com>
2025-05-12 15:35:29 +0300

    Fix: Add missing declarations for core system operations
    Added function declarations for:
    - updateAccountInfo
    - checkAccountDetails
    - makeTransaction
    - removeAccount
    - transferOwnership
    
    This resolves implicit declaration warnings during compilation.

--- src/header.h
+++ src/header.h
@@ -1,62 +1,43 @@
-#define ATM_MANAGEMENT_SYSTEM_HEADER_H
-#define ATM_MANAGEMENT_SYSTEM_HEADER_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Date structure
-struct Date {
-    int day;
-    int month;
-    int year;
+struct Date
+{
+    int month, day, year;
 };
 
-// File paths
-#define RECORDS_FILE_PATH "./data/records.txt"
-#define USERS_FILE_PATH "./data/users.txt"
-
-// Limits
-#define MAX_RECORDS 1000
-#define MAX_USERS 1000
-#define MAX_NAME_LENGTH 50
-#define MAX_PASSWORD_LENGTH 50
-#define MAX_COUNTRY_LENGTH 100
-#define MAX_ACCOUNT_TYPE_LENGTH 10
-#define MAX_PHONE_LENGTH 15
-#define MAX_ACCOUNT_NUMBER_LENGTH 20
-
-// Record structure
-struct Record {
+// all fields for each record of an account
+struct Record
+{
     int id;
     int userId;
-    char name[MAX_NAME_LENGTH];
-    char country[MAX_COUNTRY_LENGTH];
-    char phone[MAX_PHONE_LENGTH];
-    char accountType[MAX_ACCOUNT_TYPE_LENGTH];
-    char accountNumber[MAX_ACCOUNT_NUMBER_LENGTH];
+    char name[100];
+    char country[100];
+    int phone;
+    char accountType[10];
+    int accountNbr;
     double amount;
-    struct Date depositDate;
-    struct Date withdrawDate;
+    struct Date deposit;
+    struct Date withdraw;
 };
 
-// User structure
-struct User {
+struct User
+{
     int id;
-    char name[MAX_NAME_LENGTH];
-    char password[MAX_PASSWORD_LENGTH];
+    char name[50];
+    char password[50];
 };
 
-// Authentication functions
+// authentication functions
 void loginMenu(char a[50], char pass[50]);
 void registerMenu(char a[50], char pass[50]);
 const char *getPassword(struct User u);
 
-// System functions
+// system function
 void createNewAcc(struct User u);
 void mainMenu(struct User u);
 void checkAllAccounts(struct User u);
-void registerUser(struct User *u);
 void updateAccountInfo(struct User u);
 void checkAccountDetails(struct User u);
 void makeTransaction(struct User u);
